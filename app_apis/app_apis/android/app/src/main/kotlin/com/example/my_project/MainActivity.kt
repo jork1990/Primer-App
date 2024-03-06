bb088662d04a4272c17f0744a102881499650530
Jorge Luis Avendaño Cervantes <42230297+jork1990@users.noreply.github.com>
2024-03-05 19:39:45 -0500

    apk

---
+++ app_apis/app_apis/android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.JorgeCode.appapis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app_videos/app_videos/android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.JorgeCode.appvideos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
